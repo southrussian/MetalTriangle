c7740b8cd68c84b1c80e40a8124f391f729404b9
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-07-14 13:59:33 +0300

    Initial Commit

---
+++ MetalVisualisation/MetalVisualisationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalVisualisationApp.swift
+//  MetalVisualisation
+//
+//  Created by Danil Peregorodiev on 14.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MetalVisualisationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
